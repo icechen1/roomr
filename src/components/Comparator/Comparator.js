20346044ccd992c93f142dafcb9d1426d3badab7
Yu Chen Hou <houyuchen66@gmail.com>
2016-09-18 08:31:58 -0700

    not sure what i changed

--- src/components/Comparator/Comparator.js
+++ src/components/Comparator/Comparator.js
@@ -140,7 +140,7 @@ class Comparator extends Component {
                         }
                       </div>
                       <div dangerouslySetInnerHTML={ this.parseFeatures(item.features) }></div>
-                      <h2> Review Analysis </h2>
+                      <h4> Review Analysis </h4>
                       { item.reviews ? this.parseReviews(item.reviews).map((item, index) => (
                         <div className={s.tag}><b>{item.word}</b> {item.count}</div>
                       )) : <p> No reviews :( </p> }
