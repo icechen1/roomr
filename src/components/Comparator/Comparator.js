c04423fecbf0abeccf744c659abad315c35f2e72
Yu Chen Hou <houyuchen66@gmail.com>
2016-09-18 01:22:42 -0700

    Sketchy review word analysis

--- src/components/Comparator/Comparator.js
+++ src/components/Comparator/Comparator.js
@@ -12,8 +12,10 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import React, { Component, PropTypes } from 'react';
 import SearchBar from '../../components/SearchBar';
 import s from './Comparator.css';
+import { REVIEW_WORDS } from './review_words.js';
 import Link from '../Link';
 import 'whatwg-fetch';
+import nlp from 'nlp_compromise';
 
 const mock = [
 
@@ -63,9 +65,44 @@ class Comparator extends Component {
       }).catch(function(ex) {
         console.error('parsing failed', ex)
       });
+  }
 
+  parseFeatures(html) {
+    let stripped = html.replace(/<(?:.|\n)*?>/gm, '')
+    let sentencesTags = nlp.text(stripped).tags();
+    console.log(sentencesTags)
+    ;
+    //return {__html: stripped};
+    // return "";
+  }
 
+  parseReviews(reviews) {
+    let out = {};
+    
+    for(let review of reviews) {
+      if(review) {
+        let sentencesRoot = nlp.text(review).root();
+        let words = nlp.text(sentencesRoot).terms();
+        words.forEach(function (word, i) {
+          if (Object.keys(REVIEW_WORDS).indexOf(word.normal) > -1) {
+                //In the array!
+                let index = Object.keys(REVIEW_WORDS).indexOf(word.normal);
+                let key = Object.keys(REVIEW_WORDS)[index];
 
+                let current = out[key];
+                if(current)
+                  out[key] = ++current;
+                else
+                  out[key] = 1;
+          }
+        });
+      }
+    }
+    let out2 = [];
+    for(let word in out) {
+      out2.push({word: word, count: out[word]});
+    }
+    return out2;
   }
 
   componentWillUnmount() {
@@ -89,19 +126,25 @@ class Comparator extends Component {
                     { item.mainImage ?
                       <img src={item.mainImage.url} alt="Product Image" />
                       : null }
-                    </div>
-                    <div className={s.featureslist}>
-                      { (item.pricingTiers) ?
-                          <div className={s.tag}><span>${item.pricingTiers[0].retailPrice}</span></div>
-                        :
-                          null
-                      }
-                      { (item.overallRating > 0) ?
-                          <div className={s.rate}><span>{item.overallRating}/5</span></div>
-                        :
-                          <div className={s.rate}><span>Unrated</span></div>
-                      }
-                      <div>{item.description}</div>
+                      <div className={s.featureslist}>
+                        { (item.pricingTiers) ?
+                            <div className={s.priceTag}>${item.pricingTiers[0].retailPrice}</div>
+                          :
+                            null
+                        }
+                        
+                        { (item.overallRating > 0) ?
+                            <div className={s.rate}><span>{item.overallRating}/5</span></div>
+                          :
+                            <div className={s.rate}><span>Unrated</span></div>
+                        }
+                      </div>
+                      <div dangerouslySetInnerHTML={ this.parseFeatures(item.features) }></div>
+                      
+                      { item.reviews && this.parseReviews(item.reviews).map((item, index) => (
+                        (index == 0) ? <h2> Review Analysis </h2> :
+                        <div>{item.word} | {item.count}</div>
+                      ))}
                     </div>
                   </div>
                   <div className={s.buyBtn}>
---
+++ src/components/Comparator/review_words.js
@@ -0,0 +1,15 @@
+export const REVIEW_WORDS = {
+    'nice': true,
+    'great': true,
+    'useful': true,
+    'durable': true,
+    'cheap': false,
+    'awesome': true,
+    'perfect': true,
+    'love': true,
+    'damage': false,
+    'break': false,
+    'bad': false,
+    'good': true,
+    'pleased': true
+}
